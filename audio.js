ba9e5b6fd52253f05347c5fc9b98403ed0bc9b26
winnienassozi@yahoo.com <winnienassozi@yahoo.com>
2022-08-09 17:28:28 +0300

    Add source

--- audio.js
+++ audio.js
@@ -2,20 +2,28 @@ Amplitude.init({
    "songs":[
       {
          "name":"What is TB?",
-         "url":"./audio/What is TB.mp3"
+         "url":"./audio/What is TB.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
       },
       {
          "name":"Signs and Symptoms of TB",
-         "url":"./audio/Signs and symptoms of TB.mp3"
+         "url":"./audio/Signs and symptoms of TB.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
       },
       
       {
          "name":"How is TB treated?",
-         "url":"./audio/How is TB treated.mp3"
+         "url":"./audio/How is TB treated.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
       },
       {
          "name":"Where to seek for help when infected",
-         "url":"./audio/Where to seek for help when infected.mp3"
+         "url":"./audio/Where to seek for help when infected.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
       }
   
    ]
